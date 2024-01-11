65a36de39fa185a53df039b47b2bdf999f89be2b
antonioraweb <antonioraweb@pop-os.mshome.net>
2024-01-11 16:10:07 -0600

    udate cert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Certificate from './components/Certificate.jsx';
-import { certificates } from './data/fakeApiData';
+
+// Definir la lista de certificados directamente en el archivo
+const certificates = [
+  { id: 1, name: 'ANA PAOLA', apellido: 'CACERES LUCIO', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0001', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  { id: 2, name: 'ROSA AMELIA', apellido: 'CAMERO CASTRO', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0002', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  { id: 3, name: 'ZULMA SARAHI', apellido: 'CARRIZALES PINEDA', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0003', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  { id: 4, name: 'JORGE ALBERTO', apellido: 'GARCIA GARCIA', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0004', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  { id: 5, name: 'ALMA IDALIA', apellido: 'GONZALEZ LOPEZ', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0005', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  { id: 6, name: 'MARIA ADRIANA', apellido: 'GONZALEZ MUÑOZ', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0006', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  { id: 7, name: 'MARLENE', apellido: 'GONZALEZ PEREZ', course: 'Diplomado: Teoría y práctica educativa en el aula de educación básica', hours: 120, folio: '0007', emision: '15 de diciembre del 2023', periodo: 'Del 15 de noviembre al 15 de diciembre de 2023' },
+  // ... Agregar más certificados según sea necesario
+];
 
 const CertificatePage = () => {
   const location = useLocation();
@@ -18,7 +29,7 @@ const CertificatePage = () => {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='certicate'>
+      <div className='certificate'>
         <h1>Validación de Certificados</h1>
         <Routes>
           <Route path="/certificate" element={<CertificatePage />} />
@@ -29,6 +40,7 @@ const App = () => {
 };
 
 export default App;
+
 
 
 
